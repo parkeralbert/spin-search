8659025af916f1c8eefc0197a5b061308c7c6426
parkeralbert <parkeralbert@parkers-mbp.home>
2020-11-20 16:36:43 -0500

    Refactored code demo, unfinished unit tests

--- spinsearch/src/SpinTest.java
+++ spinsearch/src/SpinTest.java
@@ -2,13 +2,6 @@
 import java.util.ArrayList;
 import java.util.Date;
 public class SpinTest {
-	 /**
-     * This is our test. It creates a spider (which creates spider legs) and crawls the web.
-     * 
-     * @param args
-     *            - not used
-     */
-
 
 	
 	public static void main(String[] args) throws Exception
